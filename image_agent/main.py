bb554a85d01c054b06814a4bda77acfff611ad74
Mia Ahmad <miaahmad2022@gmail.com>
2026-02-13 18:03:31 +0500

    Added new folders and updated project

---
+++ image_agent/main.py
@@ -0,0 +1,30 @@
+# from openai import OpenAI
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# client = OpenAI()
+# response = client.chat.completions.create(
+#     model="gpt-4.1-mini",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": [
+#                 {
+#                     "type": "text",
+#                     "text": "Generate a caption for this image in 30 words ",
+#                 },
+#                 {
+#                     "type": "image_url",
+#                     "image_url": {
+#                         "url": "https://images.pexels.com/photos/36076123/pexels-photo-36076123.jpeg"
+#                     },
+#                 },
+#             ],
+#         },
+#     ],
+# )
+
+# print("Response:", response.choices[0].message.content)
+
+
